d5c503ab19510bf977771e94a25b185d6753670a
cm_yamamoto_hiroki <yamamoto.hiroki@classmethod.jp>
2020-06-09 18:18:14 +0900

    prepare for processing image file

--- mediapipe/examples/desktop/multi_hand_tracking_cpu.cc
+++ mediapipe/examples/desktop/multi_hand_tracking_cpu.cc
@@ -56,8 +56,9 @@ typedef std::tuple<OutputStreamName, FilepathName,
     HandTrackingDataInfo;
 
 template <typename T>
-bool extractHandTrackingDataToFile(mediapipe::OutputStreamPoller *poller, int iLoop,
-                       FilepathName filepath, std::string output_dirpath) {
+bool extractHandTrackingDataToFile(mediapipe::OutputStreamPoller *poller,
+                                   int iLoop, FilepathName filepath,
+                                   std::string output_dirpath) {
   if (poller->QueueSize() > 0) {
     mediapipe::Packet packet;
     if (!poller->Next(&packet))
@@ -83,7 +84,8 @@ bool extractHandTrackingDataToFile(mediapipe::OutputStreamPoller *poller, int iL
 
 const std::map<std::string, HandTrackingDataInfo> HAND_TRACKING_DATA_INFO{
     {"detection",
-     {kOutputDetections, "detection", extractHandTrackingDataToFile<mediapipe::Detection>}},
+     {kOutputDetections, "detection",
+      extractHandTrackingDataToFile<mediapipe::Detection>}},
     {"landmark",
      {kOutputDetections, "landmark",
       extractHandTrackingDataToFile<mediapipe::NormalizedLandmarkList>}},
@@ -111,6 +113,168 @@ DEFINE_string(output_video_path, "",
               "Full path of where to save result (.mp4 only). "
               "If not provided, show result in a window.");
 
+cv::Mat
+executeGraph(cv::Mat input_image_raw, mediapipe::CalculatorGraph *graph,
+             mediapipe::OutputStreamPoller *poller_video,
+             std::map<std::string, mediapipe::OutputStreamPoller *> pollers,
+             std::string output_dirpath, int iLoop) {
+  // save input frame to file
+  std::ostringstream osInputFrame;
+  osInputFrame << output_dirpath << "/result/"
+               << "iLoop=" << iLoop << "_"
+               << "inputFrame"
+               << ".jpg ";
+  cv::imwrite(osInputFrame.str(), input_image_raw);
+
+  // convert color BGR to RGB
+  cv::Mat input_image;
+  cv::cvtColor(input_image_raw, input_image, cv::COLOR_BGR2RGB);
+
+  // Wrap Mat into an ImageFrame.
+  auto input_frame = absl::make_unique<mediapipe::ImageFrame>(
+      mediapipe::ImageFormat::SRGB, input_image.cols, input_image.rows,
+      mediapipe::ImageFrame::kDefaultAlignmentBoundary);
+  cv::Mat input_frame_mat = mediapipe::formats::MatView(input_frame.get());
+  input_image.copyTo(input_frame_mat);
+
+  // Send image packet into the graph.
+  size_t frame_timestamp_us =
+      (double)cv::getTickCount() / (double)cv::getTickFrequency() * 1e6;
+  graph->AddPacketToInputStream(
+      kInputStream, mediapipe::Adopt(input_frame.release())
+                        .At(mediapipe::Timestamp(frame_timestamp_us)));
+
+  // Get the graph result packet, or stop if that fails.
+  mediapipe::Packet packet_video;
+  if (!(poller_video->Next(&packet_video))) {
+    return cv::Mat();
+  }
+
+  // get output from graph
+  auto &output_video = packet_video.Get<mediapipe::ImageFrame>();
+
+  // show output image
+  // Convert back to opencv for display or saving.
+  cv::Mat output_frame_mat = mediapipe::formats::MatView(&output_video);
+  cv::cvtColor(output_frame_mat, output_frame_mat, cv::COLOR_RGB2BGR);
+
+  // save output frame to file
+  std::ostringstream osOutputFrame;
+  osOutputFrame << output_dirpath + "/result/"
+                << "iLoop=" << iLoop << "_outputFrame.jpg";
+  cv::imwrite(osOutputFrame.str(), output_frame_mat);
+
+  // extract hand-tracking-infos from the graph to files
+  for (auto const &[k, v] : HAND_TRACKING_DATA_INFO) {
+    auto &stream_name = std::get<0>(v);
+    auto &filepath = std::get<1>(v);
+    auto &func = std::get<2>(v);
+    auto poller = pollers[stream_name];
+
+    func(poller, iLoop, filepath, output_dirpath);
+  }
+
+  return output_frame_mat;
+}
+
+void RunMPPGraphImage(
+    mediapipe::CalculatorGraph *graph,
+    mediapipe::OutputStreamPoller *poller_video,
+    std::map<std::string, mediapipe::OutputStreamPoller *> pollers,
+    std::string FLAGS_input_video_path, std::string output_dirpath) {
+  auto image_frame_raw = cv::imread(FLAGS_input_video_path);
+  int iLoop = 0;
+  auto output_frame_mat = executeGraph(image_frame_raw, graph, poller_video,
+                                       pollers, output_dirpath, iLoop);
+}
+
+void RunMPPGraphVideo(
+    mediapipe::CalculatorGraph *graph,
+    mediapipe::OutputStreamPoller *poller_video,
+    std::map<std::string, mediapipe::OutputStreamPoller *> pollers,
+    std::string FLAGS_input_video_path, std::string output_dirpath) {
+
+  LOG(INFO) << "Initialize the camera or load the video.";
+  cv::VideoCapture capture;
+  const bool load_video = !FLAGS_input_video_path.empty();
+  if (load_video) {
+    capture.open(FLAGS_input_video_path);
+  } else {
+    capture.open(0);
+  }
+  if (!capture.isOpened()) {
+    return;
+  };
+
+  cv::VideoWriter writer;
+  const bool save_video = !FLAGS_output_video_path.empty();
+  if (!save_video) {
+    cv::namedWindow(kWindowName, /*flags=WINDOW_AUTOSIZE*/ 1);
+#if (CV_MAJOR_VERSION >= 3) && (CV_MINOR_VERSION >= 2)
+    capture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    capture.set(cv::CAP_PROP_FPS, 30);
+#endif
+  } else {
+    LOG(INFO) << "Prepare video writer.";
+    writer.open(FLAGS_output_video_path,
+                mediapipe::fourcc('a', 'v', 'c', '1'), // .mp4
+                capture.get(cv::CAP_PROP_FPS),
+                cv::Size(capture.get(cv::CAP_PROP_FRAME_HEIGHT),
+                         capture.get(cv::CAP_PROP_FRAME_WIDTH)));
+    if (!writer.isOpened()) {
+      return;
+    }
+  }
+
+  LOG(INFO) << "Start grabbing and processing frames.";
+  bool grab_frames = true;
+
+  int iLoop = 0;
+
+  while (grab_frames) {
+    std::cout << "iLoop: " << iLoop
+              << "-------------------------------------------------------------"
+              << std::endl;
+    std::cerr << "iLoop: " << iLoop
+              << "-------------------------------------------------------------"
+              << std::endl;
+    // Capture opencv camera or video frame.
+    cv::Mat camera_frame_raw;
+    capture >> camera_frame_raw;
+    if (camera_frame_raw.empty())
+      break; // End of video.
+
+    std::cerr << "camera" << std::endl;
+
+    if (!load_video) {
+      cv::flip(camera_frame_raw, camera_frame_raw, /*flipcode=HORIZONTAL*/ 1);
+    }
+    auto output_frame_mat = executeGraph(camera_frame_raw, graph, poller_video,
+                                         pollers, output_dirpath, iLoop);
+
+    std::cerr << "load" << std::endl;
+    if (save_video) {
+      writer.write(output_frame_mat);
+    } else {
+      cv::imshow(kWindowName, output_frame_mat);
+      // Press any key to exit.
+      const int pressed_key = cv::waitKey(5);
+      if (pressed_key >= 0 && pressed_key != 255)
+        grab_frames = false;
+    }
+
+    std::cerr << "grab" << std::endl;
+
+    std::cout << std::endl;
+    ++iLoop;
+  }
+
+  LOG(INFO) << "Shutting down.";
+  if (writer.isOpened())
+    writer.release();
+}
+
 ::mediapipe::Status RunMPPGraph() {
   std::string calculator_graph_config_contents;
   MP_RETURN_IF_ERROR(mediapipe::file::GetContents(
@@ -125,28 +289,6 @@ DEFINE_string(output_video_path, "",
   mediapipe::CalculatorGraph graph;
   MP_RETURN_IF_ERROR(graph.Initialize(config));
 
-  LOG(INFO) << "Initialize the camera or load the video.";
-  cv::VideoCapture capture;
-  const bool load_video = !FLAGS_input_video_path.empty();
-  if (load_video) {
-    capture.open(FLAGS_input_video_path);
-  } else {
-    capture.open(0);
-  }
-  RET_CHECK(capture.isOpened());
-
-  cv::VideoWriter writer;
-  const bool save_video = !FLAGS_output_video_path.empty();
-  if (!save_video) {
-    cv::namedWindow(kWindowName, /*flags=WINDOW_AUTOSIZE*/ 1);
-#if (CV_MAJOR_VERSION >= 3) && (CV_MINOR_VERSION >= 2)
-    capture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-    capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-    capture.set(cv::CAP_PROP_FPS, 30);
-#endif
-  }
-
-  LOG(INFO) << "Start running the calculator graph.";
   // Connect pollers to graph
   ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller_video,
                    graph.AddOutputStreamPoller(kOutputVideo));
@@ -172,13 +314,6 @@ DEFINE_string(output_video_path, "",
   pollers[kOutputHandRectsFromLandmarks] = &poller_hand_rects_from_landmarks;
   pollers[kOutputLandmarksRaw] = &poller_landmarks_raw;
 
-  MP_RETURN_IF_ERROR(graph.StartRun({}));
-
-  LOG(INFO) << "Start grabbing and processing frames.";
-  bool grab_frames = true;
-
-  int iLoop = 0;
-
   // define output folder path
   std::string output_dirname =
       FLAGS_output_video_path.substr(0, FLAGS_output_video_path.rfind("/"));
@@ -189,99 +324,19 @@ DEFINE_string(output_video_path, "",
   mkdir(output_dirpath.c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
   mkdir((output_dirpath + "/result/").c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
 
-  while (grab_frames) {
-    std::cout << "iLoop: " << iLoop
-              << "-------------------------------------------------------------"
-              << std::endl;
-    std::cerr << "iLoop: " << iLoop
-              << "-------------------------------------------------------------"
-              << std::endl;
-    // Capture opencv camera or video frame.
-    cv::Mat camera_frame_raw;
-    capture >> camera_frame_raw;
-    if (camera_frame_raw.empty())
-      break; // End of video.
-    cv::Mat camera_frame;
-    cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB);
-    if (!load_video) {
-      cv::flip(camera_frame, camera_frame, /*flipcode=HORIZONTAL*/ 1);
-    }
+  LOG(INFO) << "Start running the calculator graph.";
 
-    // save input frame to file
-    std::ostringstream osInputFrame;
-    osInputFrame << output_dirpath << "/result/"
-                 << "iLoop=" << iLoop << "_inputFrame.jpg";
-    cv::imwrite(osInputFrame.str(), camera_frame_raw);
+  MP_RETURN_IF_ERROR(graph.StartRun({}));
 
-    // Wrap Mat into an ImageFrame.
-    auto input_frame = absl::make_unique<mediapipe::ImageFrame>(
-        mediapipe::ImageFormat::SRGB, camera_frame.cols, camera_frame.rows,
-        mediapipe::ImageFrame::kDefaultAlignmentBoundary);
-    cv::Mat input_frame_mat = mediapipe::formats::MatView(input_frame.get());
-    camera_frame.copyTo(input_frame_mat);
-
-    // Send image packet into the graph.
-    size_t frame_timestamp_us =
-        (double)cv::getTickCount() / (double)cv::getTickFrequency() * 1e6;
-    MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(
-        kInputStream, mediapipe::Adopt(input_frame.release())
-                          .At(mediapipe::Timestamp(frame_timestamp_us))));
-
-    mediapipe::Packet packet_video;
-    if (poller_video.Next(&packet_video)) {
-      // Get the graph result packet, or stop if that fails.
-
-      // get output from graph
-      auto &output_video = packet_video.Get<mediapipe::ImageFrame>();
-
-      // show output image
-      // Convert back to opencv for display or saving.
-      cv::Mat output_frame_mat = mediapipe::formats::MatView(&output_video);
-      cv::cvtColor(output_frame_mat, output_frame_mat, cv::COLOR_RGB2BGR);
-      if (save_video) {
-        if (!writer.isOpened()) {
-          LOG(INFO) << "Prepare video writer.";
-          writer.open(FLAGS_output_video_path,
-                      mediapipe::fourcc('a', 'v', 'c', '1'), // .mp4
-                      capture.get(cv::CAP_PROP_FPS), output_frame_mat.size());
-          RET_CHECK(writer.isOpened());
-        }
-        writer.write(output_frame_mat);
-      } else {
-        cv::imshow(kWindowName, output_frame_mat);
-        // Press any key to exit.
-        const int pressed_key = cv::waitKey(5);
-        if (pressed_key >= 0 && pressed_key != 255)
-          grab_frames = false;
-      }
-
-      // save output frame to file
-      std::ostringstream osOutputFrame;
-      osOutputFrame << output_dirpath + "/result/"
-                    << "iLoop=" << iLoop << "_outputFrame.jpg";
-      cv::imwrite(osOutputFrame.str(), output_frame_mat);
-
-    } else {
-      break;
-    }
-
-    // extract hand-tracking-infos from the graph to files
-    for (auto const &[k, v] : HAND_TRACKING_DATA_INFO) {
-      auto &stream_name = std::get<0>(v);
-      auto &filepath = std::get<1>(v);
-      auto &func = std::get<2>(v);
-      auto poller = pollers[stream_name];
-
-      func(poller, iLoop, filepath, output_dirpath);
-    }
-
-    std::cout << std::endl;
-    ++iLoop;
+  mediapipe::Status status;
+  if (false) {
+    RunMPPGraphImage(&graph, &poller_video, pollers, FLAGS_input_video_path,
+                     output_dirpath);
+  } else {
+    RunMPPGraphVideo(&graph, &poller_video, pollers, FLAGS_input_video_path,
+                     output_dirpath);
   }
 
-  LOG(INFO) << "Shutting down.";
-  if (writer.isOpened())
-    writer.release();
   MP_RETURN_IF_ERROR(graph.CloseInputStream(kInputStream));
   return graph.WaitUntilDone();
 }
